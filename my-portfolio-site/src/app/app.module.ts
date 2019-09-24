e72358aa333f148c9914bcd72162d0b21b5c22d0
Elisabeth Strunk <e.strunk@salt-and-pepper.eu>
2019-09-24 17:29:59 +0200

    feature: added content to About section

--- my-portfolio-site/src/app/app.module.ts
+++ my-portfolio-site/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -19,7 +20,8 @@ import { ContentService } from './services/content.service';
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatGridListModule
   ],
   providers: [ContentService],
   bootstrap: [AppComponent]
